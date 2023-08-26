8a18358aacb85f7a452736adaa5dcde589d4db7e
pearlKinn <jinju000202@naver.com>
2023-08-27 02:55:24 +0900

    배포페이지 경로 설정

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,7 @@ const idDev = env.NODE_ENV
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/react-homework-mission03/',
   plugins: [react()],
   css: {
     devSourcemap: true,
